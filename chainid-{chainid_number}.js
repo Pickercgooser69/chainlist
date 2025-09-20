680b5b6ac087a407abc82f931fb37d4f12afd51d
Pickercgooser69 <132561071+Pickercgooser69@users.noreply.github.com>
2025-09-20 04:17:17 -0700

    Create chainid-{chainid_number}.js

---
+++ chainid-{chainid_number}.js
@@ -0,0 +1,25 @@
+{
+  "name": "Ethereum Mainnet",
+  "chain": "ETH",
+  "rpc": [
+    "https://rpc.buildbear.io/silent-mirage-5ab24c81",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://ethereum.org",
+  "shortName": "eth",
+  "chainId": 1,
+  "networkId": 1,
+  "icon": "ethereum",
+  "explorers": [{
+    "name": "silent-mirage-5ab24c81",
+    "url": "https://explorer.buildbear.io/silent-mirage-5ab24c81",
+    "icon": "etherscan",
+    "standard": "EIP3091"
+  }]
+}
